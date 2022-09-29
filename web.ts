0cf6247a883f0f1b35b8c2d2e8165d251a22d38e
Luke Harries <lharries@users.noreply.github.com>
2022-09-29 18:52:32 +0200

    adding to main web.ts

--- web.ts
+++ web.ts
@@ -1,52 +1,151 @@
-const style = ` 
-.form, .button, .thanks {
-  position: fixed;
-  bottom: 20px;
-  right: 20px;
-  background: blue;
-  color: black;
-  font-weight: normal;
-  font-family: -apple-system, BlinkMacSystemFont, "Inter", "Segoe UI", "Roboto", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-  text-align: left;
-}
+const style = `
+        .form, .button, .thanks {
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        color: black;
+        font-weight: normal;
+        font-family: -apple-system, BlinkMacSystemFont, "Inter", "Segoe UI", "Roboto", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        text-align: left;
+        }
+        .button {
 .button { 
-  width: 64px;
-  height: 64px;
-  border-radius: 100%;
-  text-align: center;
-  line-height: 60px;
-  font-size: 32px;
-  border: none;
-  cursor: pointer;
-}
-.button:hover {
-  background: #b88505;
-}
-.form {
-  display: none;
-  padding: 20px;
-  flex-direction: column;
-}
-.form textarea {
-  margin-bottom: 20px;
-  background: white;
-  color: black;
-  border: none;
-}
-.form button {
-  color: white;
-  background: black;
-}
-.thanks {
-  display:none;
-  padding: 20px;
-}
-`;
+        .button {
+        width: 64px;
+        height: 64px;
+        border-radius: 100%;
+        text-align: center;
+        line-height: 60px;
+        font-size: 32px;
+        border: none;
+        cursor: pointer;
+        }
+        .button:hover {
+        background: #b88505;
+        }
+        .form {
+        display: none;
+        flex-direction: column;
+        border: 1px solid #f0f0f0;
+        border-radius: 8px;
+        padding-top: 5px;
+        width: 380px;
+        height: 220px;
+        box-shadow: -6px 0 16px -8px rgb(0 0 0 / 8%), -9px 0 28px 0 rgb(0 0 0 / 5%), -12px 0 48px 16px rgb(0 0 0 / 3%);
+        }
+        .form textarea {
+            color: #2d2d2d;
+            font-size: 14px;
+            font-family: -apple-system, BlinkMacSystemFont, "Inter", "Segoe UI", "Roboto", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        margin-bottom: 10px;
+        background: white;
+        color: black;
+        border: none;
+        outline: none;
+        padding-left: 10px;
+        padding-right: 10px;
+        padding-top: 10px;
+        }
+        .form-submit {
+            box-sizing: border-box;
+            margin: 0;
+            font-family: inherit;
+            overflow: visible;
+            text-transform: none;
+            line-height: 1.5715;
+            position: relative;
+            display: inline-block;
+            font-weight: 400;
+            white-space: nowrap;
+            text-align: center;
+            border: 1px solid transparent;
+            cursor: pointer;
+            transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+            user-select: none;
+            touch-action: manipulation;
+            height: 32px;
+            padding: 4px 15px;
+            font-size: 14px;
+            border-radius: 4px;
+            outline: 0;
+            color: #fff;
+            border-color: #1d4aff;
+            background: #1d4aff;
+            text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.12);
+            box-shadow: 0 2px 0 rgba(0, 0, 0, 0.045);
+        }
+        .form-cancel {
+            box-sizing: border-box;
+            margin: 0;
+            font-family: inherit;
+            overflow: visible;
+            text-transform: none;
+            line-height: 1.5715;
+            position: relative;
+            display: inline-block;
+            font-weight: 400;
+            white-space: nowrap;
+            text-align: center;
+            border: 1px solid transparent;
+            box-shadow: 0 2px 0 rgba(0, 0, 0, 0.015);
+            cursor: pointer;
+            transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+            user-select: none;
+            touch-action: manipulation;
+            height: 32px;
+            padding: 4px 15px;
+            font-size: 14px;
+            border-radius: 4px;
+            color: #2d2d2d;
+            border-color: rgba(0, 0, 0, 0.15);
+            background: #fff;
+            outline: 0;
+        }
+        .thanks {
+            display: none;
+            font-size: 14px;
+            padding: 20px;
+            border: 1px solid #f0f0f0;
+            border-radius: 8px;
+            box-shadow: -6px 0 16px -8px rgb(0 0 0 / 8%), -9px 0 28px 0 rgb(0 0 0 / 5%), -12px 0 48px 16px rgb(0 0 0 / 3%);
+            width: 340px;
+            margin-block-end: 1em;
+        }
+        .bolded { font-weight: 600; }
+        .bottom-section {
+            border-top: 1px solid #f0f0f0;
+            padding: 10px 16px;
+        }
+        .buttons {
+            display: flex;
+            justify-content: space-between;
+        }
+        .specific-issue {
+            padding-top: 10px;
+            font-size: 14px;
+            color: #747ea1;
+            font-family: -apple-system, BlinkMacSystemFont, "Inter", "Segoe UI", "Roboto", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        }
+        .specific-issue a:link {
+            color: #5879FF;
+        }
+        .specific-issue a:visited {
+            color: #5879FF;
+        }
+        `;
 
 const form = `
-  <textarea name='feedback' rows=6></textarea>
-  <button class='form-submit' type='submit'></button>
-`;
+        <textarea class='feedback-textarea' name='feedback' rows=6 placeholder="Help us improve"></textarea>
+        <div class='bottom-section'>
+            <div class='buttons'>
+                <a class='form-cancel' type='button'>Close</a>
+                <button class='form-submit' type='submit'>Submit</button>
+            </div>
+            <div class='specific-issue'>
+                <strong class='bolded'>Have a specific issue?</strong> Contact <a class="support-text" href="mailto:hey@posthog.com">Posthog Support</a> or <a class="support-text" href="https://posthog.com/docs">checkout our docs</a>
+            </div>
+        </div>
+        `;
 
 export function inject({ config, posthog }) {
   const shadow = createShadow();
@@ -54,11 +153,17 @@ export function inject({ config, posthog }) {
   function openFeedbackBox() {
     Object.assign(buttonElement.style, { display: "none" });
     Object.assign(formElement.style, { display: "flex" });
-    const submit: HTMLElement | undefined =
-      formElement.querySelector(".form-submit");
+
+    const submit = formElement.querySelector(".form-submit");
     if (submit) {
       submit.innerText = config.sendButtonText;
     }
+
+    const closeButton = shadow.querySelector(".form-cancel");
+    closeButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      Object.assign(formElement.style, { display: "none" });
+    });
   }
 
   const buttonElement = Object.assign(document.createElement("button"), {
@@ -80,6 +185,7 @@ export function inject({ config, posthog }) {
       window.setTimeout(() => {
         Object.assign(thanksElement.style, { display: "none" });
       }, 3000);
+      formElement.reset();
     },
   });
   shadow.appendChild(formElement);
@@ -93,7 +199,7 @@ export function inject({ config, posthog }) {
 
   const thanksElement = Object.assign(document.createElement("div"), {
     className: "thanks",
-    innerHTML: config.thanksText || "Thank you! Closing in 3 seconds...",
+    innerHTML: "<div>" + config.thanksText + "</div>" || "Thank you!",
   });
   shadow.appendChild(thanksElement);
 }
